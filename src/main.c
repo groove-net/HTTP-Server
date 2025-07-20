c4a979296eb111e6ab7d9ae71c8ed232a4593333
Gabriel Adelemoni <gabadelemoni@outlook.com>
2025-07-19 23:40:46 -0400

    Initial Commit

--- src/main.c
+++ src/main.c
@@ -3,9 +3,7 @@
  Name        : HTTP Server
  Author      : Gabriel Adelemoni
  Version     : 1.0
- Description : A simple C application demonstrating project structure with
-               src, include, Makefile-based build system, and modular design.
-               Uses static libraries and includes dependency management.
+ Description : An event-driven HTTP Server written in pure C.
  License     : MIT
  ============================================================================
 */
